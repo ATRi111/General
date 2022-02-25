0115807974e5059844c3aba9859da3aa81d76cc4
ATRi111 <1657125180@qq.com>
2022-02-25 10:23:22 +0800

    实现异步加载

--- Assets/Scripts/System/LoadManager.cs
+++ Assets/Scripts/System/LoadManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,12 +7,19 @@ public class LoadManager : Service
 {
     private EventSystem eventSystem;
 
-    [Header("主菜单的场景号")]
-    public int index_menu;
-    [Header("最大场景号")]
-    public int index_max;
-    [Header("是否异步加载")]
+    [SerializeField]
+    private int index_menu;
+    [SerializeField]
+    private int index_max;
+    /// <summary>
+    /// 是否异步加载场景
+    /// </summary>
     public bool asynchronous;
+
+    /// <summary>
+    /// 异步加载场景的事件
+    /// </summary>
+    public event Action<AsyncOperation> AsyncLoadScene;
 
     [SerializeField]
     private int _Index;
@@ -25,37 +34,59 @@ public class LoadManager : Service
                 return;
             _Index = value;
             eventSystem.ActivateEvent(EEvent.BeforeLoadScene, value);
-            SceneManager.LoadScene(value);
+            StartCoroutine(LoadSceneProcess(value, asynchronous));
         }
     }
 
     protected override void BeforeRegister()
     {
-        eService = EService.SceneManager;
+        eService = EService.LoadManager;
         eventSystem = ServiceLocator.Instance.GetService<EventSystem>(EService.EventSystem);
+        index_max = SceneManager.sceneCountInBuildSettings;
+        index_menu = SceneManager.GetSceneByName("menu").buildIndex;
     }
 
     //禁止用不属于本类的方法加载场景
-    public void LoadScene(int index)
+    public void LoadLevel(int index)
     {
         Index = index;
     }
-    public void LoadNextScene()
+    public void LoadNextLevel()
     {
         Index++;
     }
-
-    public void Exit()
+    public void RetuenToMenu()
     {
         Index = index_menu;
     }
-
     public void Quit()
     {
-        #if UNITY_EDITOR
-            UnityEditor.EditorApplication.isPlaying = false;
-        #else
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
             Application.Quit();
-        #endif
+#endif
+    }
+
+    /// <param name="confirm">加载完成后，是否需要确认</param>
+    private IEnumerator LoadSceneProcess(int index, bool asynchronous, bool confirm = false)
+    {
+        if (asynchronous)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(index);
+            AsyncLoadScene?.Invoke(operation);
+            operation.allowSceneActivation = !confirm;
+            operation.allowSceneActivation = false;
+            for (; !operation.isDone;)
+            {
+                yield return null;
+            }
+        }
+        else
+        {
+            SceneManager.LoadScene(index);
+        }
+        yield return null;
+        eventSystem.ActivateEvent(EEvent.AfterLoadScene, index);
     }
 }
