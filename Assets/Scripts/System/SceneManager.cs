9c1ebfa22788d44ef465f406a2555ff2b4918844
ATRi111 <1657125180@qq.com>
2022-03-15 09:06:33 +0800

    优化代码

---
+++ Assets/Scripts/System/SceneManager.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class SceneManager : Service
+{
+    private EventSystem eventSystem;
+
+    private int index_menu;
+    private int index_max;
+    /// <summary>
+    /// 是否异步加载场景
+    /// </summary>
+    public bool asynchronous;
+
+    /// <summary>
+    /// 开始异步加载场景时，发送异步操作
+    /// </summary>
+    public event Action<AsyncOperation> AsyncLoadScene;
+
+    [SerializeField]
+    private int _Index;
+    public int Index
+    {
+        get => _Index;
+        private set
+        {
+            if (value > index_max)
+                value = index_menu;
+            if (value == _Index || value <= 0)
+                return;
+            _Index = value;
+            StartCoroutine(LoadSceneProcess(value, asynchronous));
+        }
+    }
+
+    protected override void Awake()
+    {
+        base.Awake();
+        index_max = UnityEngine.SceneManagement.SceneManager.sceneCountInBuildSettings;
+        index_menu = UnityEngine.SceneManagement.SceneManager.GetSceneByName("menu").buildIndex;
+        eventSystem = ServiceLocator.Instance.GetService<EventSystem>();
+    }
+
+    //禁止用不属于本类的方法加载场景
+    public void LoadLevel(int index)
+    {
+        Index = index;
+    }
+    public void LoadNextLevel()
+    {
+        Index++;
+    }
+    public void RetuenToMenu()
+    {
+        Index = index_menu;
+    }
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+    }
+
+    /// <param name="confirm">加载到90%时是否需要确认</param>    
+    private IEnumerator LoadSceneProcess(int index, bool async, bool confirm = false)
+    {
+        eventSystem.ActivateEvent(EEvent.BeforeLoadScene, index);
+        if (async)
+        {
+            AsyncOperation operation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync(index);
+            AsyncLoadScene?.Invoke(operation);
+            operation.allowSceneActivation = !confirm;
+            operation.allowSceneActivation = false;
+            yield return operation;
+        }
+        else
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene(index);
+        }
+        yield return null;
+        eventSystem.ActivateEvent(EEvent.AfterLoadScene, index);
+    }
+}
