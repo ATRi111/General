58561b1c597ed1f59389fb903c2cc892e7d948a1
ATRi111 <1657125180@qq.com>
2022-03-18 13:09:12 +0800

    ——

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -3,9 +3,12 @@ using UnityEditor;
 
 public class Character : MonoBehaviour
 {
-    #region 保存或撤销对角色参数的修改
-    [ContextMenuItem("保存角色参数", "Save")]
-    [ContextMenuItem("读取之前保存的角色参数", "Load")]
+
+
+    #region 保存或撤销对部分角色参数的修改
+    [Header("保存的部分角色参数")]
+    [ContextMenuItem("保存角色参数（editormode和playmode均有效）", "Save")]
+    [ContextMenuItem("读取之前保存的角色参数（editormode有效，playmode临时有效）", "Load")]
     [SerializeField]
     private CharacterData data;
     private void Save()
