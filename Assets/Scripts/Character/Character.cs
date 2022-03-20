375fc98d95b0d45faeb004fa178ee28f08b758f7
ATRi111 <1657125180@qq.com>
2022-03-20 15:19:48 +0800

    ——

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -6,18 +6,20 @@ public class Character : MonoBehaviour
     public int health;
 
     #region 保存或撤销对部分角色参数的修改
+#if UNITY_EDITOR
     [Header("保存的部分角色参数")]
     [ContextMenuItem("保存角色参数（editormode和playmode均有效）", "Save")]
     [ContextMenuItem("读取之前保存的角色参数（editormode有效，playmode临时有效）", "Load")]
     [SerializeField]
     private CharacterData data;
-    private void Save()
+    protected virtual void Save()
     {
-        Debug.Log($"角色参数已保存在{AssetDatabase.GetAssetPath(data)}");
+        Debug.Log($"{gameObject.name}的参数已保存在{AssetDatabase.GetAssetPath(data)}");
     }
-    private void Load()
+    protected virtual void Load()
     {
-        Debug.Log($"已从{AssetDatabase.GetAssetPath(data)}读取角色参数");
+        Debug.Log($"{gameObject.name}的参数已保存在{AssetDatabase.GetAssetPath(data)}");
     }
+#endif
     #endregion
 }
